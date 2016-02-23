ae1dc7f70bbcfa218f672799ac13873523b91e1b
Ray Besiga <raybesiga@gmail.com>
2016-02-23 11:33:11 +0900

    Remove unused templatetags

--- yokogao/views.py
+++ yokogao/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, HttpResponse
 import requests
-# import json
 
 def index(request):
     yoko = requests.get('https://api.github.com/users/raybesiga')
@@ -22,22 +21,3 @@ def index(request):
         yokogaoData['updated_at'] = data['updated_at']
     cleanedData.append(yokogaoData)
     return render(request, 'yokogao/index.html', {'data': yokogaoData})
-
-# def index(request):
-#     jsonList = []
-#     req = requests.get('https://api.github.com/users/raybesiga')
-#     jsonList.append(json.loads(req.content))
-#     parsedData = []
-#     userData = {}
-#     for data in jsonList:
-#         userData['name'] = data['name']
-#         userData['blog'] = data['blog']
-#         userData['email'] = data['email']
-#         userData['public_gists'] = data['public_gists']
-#         userData['public_repos'] = data['public_repos']
-#         userData['avatar_url'] = data['avatar_url']
-#         userData['followers'] = data['followers']
-#         userData['following'] = data['following']
-#     parsedData.append(userData)
-#     return render(request, 'yokogao/index.html', {'data': userData})
-#     # return HttpResponse(parsedData)
