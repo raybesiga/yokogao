f792b68842eb3a98dc4368d9ac75f48716ef62af
PeteCoward <peter.coward@gmail.com>
2016-02-23 15:57:50 +0900

    removing pythin code, fixing dates

--- yokogao/views.py
+++ yokogao/views.py
@@ -1,23 +1,12 @@
-from django.shortcuts import render, HttpResponse
+import dateutil.parser
+from django.shortcuts import render
 import requests
 
 def index(request):
     yoko = requests.get('https://api.github.com/users/raybesiga')
-    lisht = []
-    lisht.append(yoko.json())
-    cleanedData = []
-    yokogaoData = {}
-    for data in lisht:
-        yokogaoData['name'] = data['name']
-        yokogaoData['blog'] = data['blog']
-        yokogaoData['email'] = data['email']
-        yokogaoData['public_gists'] = data['public_gists']
-        yokogaoData['public_repos'] = data['public_repos']
-        yokogaoData['avatar_url'] = data['avatar_url']
-        yokogaoData['followers'] = data['followers']
-        yokogaoData['following'] = data['following']
-        yokogaoData['location'] = data['location']
-        yokogaoData['created_at'] = data['created_at']
-        yokogaoData['updated_at'] = data['updated_at']
-    cleanedData.append(yokogaoData)
-    return render(request, 'yokogao/index.html', {'data': yokogaoData})
+    context = yoko.json()
+
+    # context has a created_at key with a string value
+    # parse the string value into a python date object
+    context['created_at'] = dateutil.parser.parse(context['created_at'])
+    return render(request, 'yokogao/index.html', context)
