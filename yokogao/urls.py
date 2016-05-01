901a73542c381257b9fd5e27e7ed3fd5782d6e0e
Ray Besiga <raybesiga@gmail.com>
2016-05-01 19:22:22 +0900

    Revert "added extra functionality"

--- yokogao/urls.py
+++ yokogao/urls.py
@@ -1,11 +1,6 @@
-# -*- coding: utf-8 -*-
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'repos$', views.repo_list, name='repo_list'),
-    url(r'repo/(?P<repo_name>[\w-]+)', views.repo_detail, name='repo_detail'),
-    url(r'followers$', views.follower_list, name='follower_list'),
-    url(r'following$', views.following_list, name='following_list')
 ]
